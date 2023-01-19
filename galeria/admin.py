f5e294cbb22d85ce68234037e040dbbaef657b0a
oricardos <ricardo.rs95@gmail.com>
2023-01-19 00:03:09 -0300

    usando django admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('category',)
+    list_per_page = 10
+admin.site.register(Article, ArticleAdmin)
--- galeria/models.py
+++ galeria/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 class Article(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
+
+    TAGS_OPTIONS = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('GALAXIA', 'Galaxia'),
+    ]
+
+    title = models.CharField(max_length=255, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    image = models.CharField(max_length=255)
+    image = models.CharField(max_length=255, null=False, blank=False)
+    category = models.CharField(max_length=255, choices=TAGS_OPTIONS, default='')
 
     def __str__(self):
         return self.title
