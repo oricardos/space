ad9634db54cfb763e796583f0eaeb01a7fe7be7d
oricardos <ricardo.rs95@gmail.com>
2023-01-19 23:30:00 -0300

    add envio de arquivos

--- galeria/models.py
+++ galeria/models.py
@@ -11,7 +11,7 @@ class Article(models.Model):
     title = models.CharField(max_length=255, null=False, blank=False)
     content = models.TextField(null=False, blank=False)
     image_file = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True)
-    image_url = models.CharField(max_length=255, null=False, blank=False)
+    image_url = models.CharField(max_length=255, null=False, blank=True)
     category = models.CharField(max_length=255, choices=TAGS_OPTIONS, default='')
     is_posted = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=datetime.now, blank=False)
