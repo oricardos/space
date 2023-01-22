a22459e389904ea6bf6af22698b6ca490da871fb
oricardos <ricardo.rs95@gmail.com>
2023-01-21 23:03:51 -0300

    adicionando pesquisa

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galeria.views import index, article
+from galeria.views import index, article, search
 
 urlpatterns = [
     path('', index, name='index'),
     path('article/<int:article_id>/', article, name='article'),
+    path('search', search, name='search'),
 ]   
--- galeria/views.py
+++ galeria/views.py
@@ -8,3 +8,12 @@ def index(request):
 def article(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
     return render(request, 'galeria/article.html', {'article': article})
+
+def search(request):
+    articles = Article.objects.order_by('-created_at').filter(is_posted=True)
+    if 'search' in request.GET:
+        search_term = request.GET['search']
+        if search_term:
+            articles = articles.filter(title__icontains=search_term)
+            print(search_term, 'search term')
+    return render(request, 'search.html', {'articles': articles})
