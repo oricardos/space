bec7ca5a05821320b0cf52a348df28b39dbd2077
oricardos <ricardo.rs95@gmail.com>
2023-01-23 20:59:43 -0300

    criando rotas de login e cadastro

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import login, register
 
 urlpatterns = [
-    path()
+    path('login', login, name='login'),
+    path('register', register, name='register'),
 ]
